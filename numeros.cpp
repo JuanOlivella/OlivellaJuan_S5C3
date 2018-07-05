580f2d8d67ad34f671e544227259e9fd082e95f2
Juan Diego Olivella Cicero <jd.olivella@uniandes.edu.co>
2018-07-05 11:37:44 -0500

    Tercera parte de los ejercicios

--- numeros.cpp
+++ numeros.cpp
@@ -12,7 +12,7 @@ int main(){
   int contador = 0;
   for(i=0;i<1000;i++){
     numeroal = rand() % 100 + 1;
-    cout << i << " " << numeroal << endl;
+    
     if(numeroal%2 == 0 && numeroal < 89 &&contador < 25)
     {
       contador ++;
