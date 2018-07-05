f21c95d70e0ecb3399ff4921693815f819c2f47e
Juan Diego Olivella Cicero <jd.olivella@uniandes.edu.co>
2018-07-05 11:51:25 -0500

    Solucion final de los ejercicios

--- numeros.cpp
+++ numeros.cpp
@@ -10,8 +10,13 @@ using std::endl;
 int main(){
   int i, numeroal;
   int contador = 0;
+  int lista[1000];
+
+  cout << "La lista de numeros que cumplen la condicion es: " << endl;
+
   for(i=0;i<1000;i++){
     numeroal = rand() % 100 + 1;
+    lista[i]=numeroal; 
     
     if(numeroal%2 == 0 && numeroal < 89 &&contador < 25)
     {
@@ -19,6 +24,14 @@ int main(){
       cout << contador << ". " << numeroal << endl; 
        
     }	 	 
+  }
+  
+  cout<< "" <<endl;
+
+  cout << "La lista de numeros completos es: " << endl;
+  
+  for(i=0;i<1000;i++){
+    cout << i << ". " << lista[i] << endl;
   }	
 
   return 0;
